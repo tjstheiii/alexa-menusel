c510327bb4e70dd666ef9ac9850c0cf1d86d8d78
Thomas  Stone <tjstheiii@gmail.com>
2017-08-14 20:44:19 -0500

    updated index and added alexa-sdk

--- index.js
+++ index.js
@@ -1,39 +1,78 @@
-var alexa = require('alexa-app');
-var app = new alexa.app();
-app.launch(function(request, response){
- response.say("Hello there, I am a bot created to help you find what to eat for lunch.");
- response.shouldEndSession(false);
-})
+var Alexa = require('alexa-sdk');
 
-app.intent('GetLunchSuggestions',
- {
-  "slots": {},
-  "utterances": [
-   "what's for lunch",
-   "where should {I|we} go for lunch"
-  ]
- },
- function (request, response) {
-  generate_suggestions(response);
-  return; 
- }
-);
-function generate_suggestions(response){
- var food = ["Thai",
-  "Sushi",
-  "Chik-fil-a",
-  "Smash Burgers",
-  "Uncle Julio's"
- ];
-var rand = food[Math.floor(Math.random() * food.length)];
-response.say("How about some " + rand + " today?");
- response.send();
-return ;
-}
-// Connect to lambda
-exports.handler = app.lambda();
-if ((process.argv.length === 3) && (process.argv[2] === 'schema'))
-{
-    console.log (app.schema ());
-    console.log (app.utterances ());
-}
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context, callback);
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('HelloWorldIntent');
+    },
+
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+ };
+
+this.emit(':ask', 'What would you like to do?', 'Please say that again?');
+
+var speechOutput = 'Hello world!';
+var repromptSpeech = 'Hello again!';
+
+this.emit(':tell', speechOutput);
+
+this.emit(':ask', speechOutput, repromptSpeech);
+
+var cardTitle = 'Hello World Card';
+var cardContent = 'This text will be displayed in the companion app card.';
+
+var imageObj = {
+    smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
+    largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png'
+};
+
+var permissionArray = ['read::alexa:device:all:address'];
+
+var updatedIntent = this.event.request.intent;
+
+var slotToElicit = "Slot to elicit";
+
+var slotToConfirm = "Slot to confirm";
+
+this.emit(':askWithCard', speechOutput, repromptSpeech, cardTitle, cardContent, imageObj);
+
+this.emit(':tellWithCard', speechOutput, cardTitle, cardContent, imageObj);
+
+this.emit(':tellWithLinkAccountCard', speechOutput);
+
+this.emit(':askWithLinkAccountCard', speechOutput);
+
+this.emit(':tellWithPermissionCard', speechOutput, permissionArray);
+
+this.emit(':delegate', updatedIntent);
+
+this.emit(':elicitSlot', slotToElicit, speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':elicitSlotWithCard', slotToElicit, speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':confirmSlot', slotToConfirm, speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':confirmSlotWithCard', slotToConfirm, speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':confirmIntent', speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':confirmIntentWithCard', speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState. Can be overridden.
+
+this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to 'true' to force saving.
+
+this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
+
+
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
